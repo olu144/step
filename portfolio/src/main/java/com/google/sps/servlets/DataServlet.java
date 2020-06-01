d695897a0b2ead3ef21d9e4c333359f78d665da9
Olu <olu144@gmail.com>
2020-06-01 13:30:40 -0400

    part 2 complete(fetch)

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,7 +27,7 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Olu!</h1>");
+    response.getWriter().println("Hello Olu!");
   }
 }
 
