55514bfec702043c250986b7d8cb163c705cc610
olu144 <54452415+olu144@users.noreply.github.com>
2020-06-03 18:42:13 -0400

    3.5.part2.improved (#9)
    * complete step 1(servlets)
    
    * complete step 2(fetch)
    
    * part 2 complete(fetch)
    
    * modify DataServlet, script and index to complete step 3
    
    * complete step 4 by implementing form on contact page
    
    * complete the first part of week 3 step 5
    
    * Address requests made in 3.3
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * just in case
    
    * improved 3.5 with the addition of a comment class
    
    * address nits
    
    Co-authored-by: Olu <olu144@gmail.com>

---
+++ portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -0,0 +1,12 @@
+package com.google.sps.servlets;
+public class Comment {
+  private long id;
+  private String comment;
+  private long timestamp;
+
+  public Comment(long id, String comment, long timestamp){
+    this.id = id;
+    this.comment = comment;
+    this.timestamp = timestamp;
+  }
+}
--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,12 +13,22 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
-
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.util.List;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -26,8 +36,34 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Olu!");
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    ArrayList<Comment>comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long tempId = entity.getKey().getId();
+      String tempComment = (String) entity.getProperty("comment");
+      long tempTimestamp = (long) entity.getProperty("timestamp");
+      if(tempComment != null && tempComment.strip() != ""){
+        Comment comment = new Comment(tempId, tempComment, tempTimestamp);
+        comments.add(comment);
+      }
+    }
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String comment = request.getParameter("comment");
+    long timestamp = System.currentTimeMillis();
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("comment", comment);
+    commentEntity.setProperty("timestamp", timestamp);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
+    response.sendRedirect("comments.html");
   }
 }
 
