ef0029ccff75f19c355232695791063e1922da18
Olu <olu144@gmail.com>
2020-06-09 17:21:11 +0000

    add map viewer in maps.html and navbar tab in index.html

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -124,3 +124,10 @@ function drawChart2() {
     chart.draw(data, options);
   });
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
