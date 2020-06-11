27193816a3caa4d6bc999a669f93885f220e0532
olu144 <54452415+olu144@users.noreply.github.com>
2020-06-10 22:55:43 -0400

    Login status (#12)
    * complete step 1(servlets)
    
    * complete step 2(fetch)
    
    * part 2 complete(fetch)
    
    * modify DataServlet, script and index to complete step 3
    
    * complete step 4 by implementing form on contact page
    
    * complete the first part of week 3 step 5
    
    * Address requests made in 3.3
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * just in case
    
    * improved 3.5 with the addition of a comment class
    
    * just in case
    
    * complete week 3 step 6(additional features)
    
    * complete week 3 step 6(additional features)
    
    * formatting improvements
    
    * formatting improvements
    
    * address nits
    
    * add space
    
    * working form and minor UI tweaks
    
    * Week 4 login status servlet
    
    * improved HomeServlet
    
    * formatting
    
    * formatting
    
    * formatting
    
    * address nits
    
    Co-authored-by: Olu <olu144@gmail.com>

---
+++ portfolio/src/main/java/com/google/sps/servlets/HomeServlet.java
@@ -0,0 +1,24 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+
+@WebServlet("/login-status")
+public class HomeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    boolean isLoggedIn = false;
+    UserService userService = UserServiceFactory.getUserService();
+    isLoggedIn = userService.isUserLogggedIn();
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(isLoggedIn));
+  }
+}
