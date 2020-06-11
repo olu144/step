90c8b86a90244ed77a39dafa9a3cc978473c3516
olu144 <54452415+olu144@users.noreply.github.com>
2020-06-11 18:47:01 -0400

    Chart.1 (#15)
    * complete step 1(servlets)
    
    * complete step 2(fetch)
    
    * part 2 complete(fetch)
    
    * modify DataServlet, script and index to complete step 3
    
    * complete step 4 by implementing form on contact page
    
    * complete the first part of week 3 step 5
    
    * Address requests made in 3.3
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * just in case
    
    * improved 3.5 with the addition of a comment class
    
    * just in case
    
    * complete week 3 step 6(additional features)
    
    * complete week 3 step 6(additional features)
    
    * formatting improvements
    
    * formatting improvements
    
    * address nits
    
    * add space
    
    * working form and minor UI tweaks
    
    * Week 4 login status servlet
    
    * improved HomeServlet
    
    * formatting
    
    * Viewing comments not requires a login
    
    * E-Mails are now displayed alongside comments
    
    * formatting
    
    * implemented chart in chart.html
    
    * formatting
    
    * spelling
    
    * created loadChartsApi function
    
    * remove unnecessary async
    
    Co-authored-by: Olu <olu144@gmail.com>

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -78,3 +78,29 @@ async function isLoggedIn() {
   const status = await response.json();
   return status;
 }
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+  data.addRows([
+    ['Lions', 10],
+    ['Tigers', 5],
+    ['Bears', 15],
+    ['Elephants', 4],
+    ['Snakes', 8]
+  ]);
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+  const chart = new google.visualization.PieChart(document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
+function loadChartsApi() {
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+}
