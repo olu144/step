ebb93e01ce37d18273ad60ca9b227daccd1ed844
Olu <olu144@gmail.com>
2020-06-12 20:22:25 +0000

    optimize loadChartsApi function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -123,11 +123,7 @@ function drawChart2() {
 }
 
 function loadChartsApi() {
-  google.charts.load('current', {'packages':['corechart']});
+  google.charts.load('current', {'packages':['corechart', 'line']});
   google.charts.setOnLoadCallback(drawChart);
-}
-
-function loadChartsApi2() {
-  google.charts.load('current', {'packages':['line']});
   google.charts.setOnLoadCallback(drawChart2);
 }
