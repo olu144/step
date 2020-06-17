c3d9d197bc4dd56a7c056fb2fe6fc7e5e87d0887
Olu <olu144@gmail.com>
2020-06-17 18:21:25 +0000

    ignoresPeopleNotAttending bug

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,68 +24,67 @@ import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    long duration = request.getDuration();
-    Collection<String> attendees = request.getAttendees();
+    long requestDuration = request.getDuration();
+    Collection<String> requestAttendees = request.getAttendees();
     ArrayList<TimeRange> validTimes = new ArrayList<TimeRange>();
-    ArrayList<TimeRange> output = new ArrayList<TimeRange>();  
-    ArrayList<TimeRange> finalOutput = new ArrayList<TimeRange>();  
+    ArrayList<TimeRange> noOverlap = new ArrayList<TimeRange>();  
+    ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();  
     int newStart;
     int newEnd;
-
-    if (attendees.isEmpty()) {
+    //edge case: if there are no attendees the whole day is returned        
+    if (requestAttendees.isEmpty()) {
       return Arrays.asList(TimeRange.WHOLE_DAY);
     }
-
-    if (duration >= TimeRange.WHOLE_DAY.duration()) {
+    //edge case: if the requested meeting is longer than a day there are no options
+    if (requestDuration >= TimeRange.WHOLE_DAY.duration()) {
       return Arrays.asList();  
     }
-
+    //add the times for all meetings where there are attendees
     for (Event e: events) {
       if (!e.getAttendees().isEmpty()) {
         validTimes.add(e.getWhen());
-        }
+      }
+    }
+    //if there are no meetings with attendees the whole day is returned 
+    if(validTimes.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);    
     }  
-
+    //if there are multiple meetings consolidate overlapping meetings into one range of time
     if (validTimes.size() > 1){
       for (int i = 1; i < validTimes.size(); i++){
-        if (validTimes.get(i-1).overlaps(validTimes.get(i))){
-          newStart = Math.min(validTimes.get(i-1).start(), validTimes.get(i).start());
-          newEnd = Math.max(validTimes.get(i).end(), validTimes.get(i-1).end());
-          output.add(TimeRange.fromStartEnd(newStart, newEnd, false));
+        if (validTimes.get(i - 1).overlaps(validTimes.get(i))){
+          newStart = Math.min(validTimes.get(i - 1).start(), validTimes.get(i).start());
+          newEnd = Math.max(validTimes.get(i).end(), validTimes.get(i - 1).end());
+          noOverlap.add(TimeRange.fromStartEnd(newStart, newEnd, false));
         } else {
-          output.add(validTimes.get(i-1));
-          output.add(validTimes.get(i));
+          noOverlap.add(validTimes.get(i - 1));
+          noOverlap.add(validTimes.get(i));
         }
       }
     } else {
-      output = validTimes;
+      noOverlap = validTimes;
     }
-
-    if(output.isEmpty()) {
-      return Arrays.asList(TimeRange.WHOLE_DAY);    
-    }
-
-    if(output.get(0).start() - TimeRange.START_OF_DAY >= duration) {
+    //if there is adequate time between the beginning of the day and the beginning of the first meeting add that time range to the output list   
+    if(noOverlap.get(0).start() - TimeRange.START_OF_DAY >= requestDuration) {
       newStart = TimeRange.START_OF_DAY;
-      newEnd = output.get(0).start();
-      finalOutput.add(TimeRange.fromStartEnd(newStart, newEnd, false));    
+      newEnd = noOverlap.get(0).start();
+      availableTimes.add(TimeRange.fromStartEnd(newStart, newEnd, false));    
     }
-
-    for (int i = 0; i < output.size() - 1; i++) {
-      if(output.get(i+1).start() - output.get(i).end() >= duration){
-          newStart = output.get(i).end();
-          newEnd = output.get(i+1).start();
-          finalOutput.add(TimeRange.fromStartEnd(newStart, newEnd, false));
+    //if there are adequate time between meetings add those time ranges to the output list
+    for (int i = 0; i < noOverlap.size() - 1; i++) {
+      if(noOverlap.get(i + 1).start() - noOverlap.get(i).end() >= requestDuration){
+          newStart = noOverlap.get(i).end();
+          newEnd = noOverlap.get(i + 1).start();
+          availableTimes.add(TimeRange.fromStartEnd(newStart, newEnd, false));
       }
     }
-
-    if(TimeRange.END_OF_DAY - output.get(output.size() - 1).end() >= duration) {
-        newStart = output.get(output.size()-1).end();
+    //if there is adequate time between the end of the day and the end of the last meeting add that time range to the output list   
+    if(TimeRange.END_OF_DAY - noOverlap.get(noOverlap.size() - 1).end() >= requestDuration) {
+        newStart = noOverlap.get(noOverlap.size() - 1).end();
         newEnd = TimeRange.END_OF_DAY;
-      finalOutput.add(TimeRange.fromStartEnd(newStart,newEnd , true));
+      availableTimes.add(TimeRange.fromStartEnd(newStart,newEnd , true));
     }
-
-    return finalOutput;
-
+    //return the list of available time ranges
+    return availableTimes;
   }
 }
