e55b35bf34400971e40178d0005d794b19713ae9
Olu <olu144@gmail.com>
2020-06-17 01:09:17 +0000

    tdd

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,37 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    long duration = request.getDuration();
+    Collection<String> attendees = request.getAttendees();
+    Collection<String> optionalAttendees = request.getOptionalAttendees();
+    ArrayList<Event> validEvents = new ArrayList<Event>();
+    ArrayList<TimeRange> validTimes = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> output = new ArrayList<TimeRange>();
+    if (attendees.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    if (duration >= TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();  
+    }
+    for (Event e: events) {
+      if (!e.getAttendees().isEmpty()) {
+        validTimes.add(e.getWhen());
+        }
+    }
+    Collections.sort(validTimes, TimeRange.ORDER_BY_START);
+    for (int i = 1; i < validTimes.size(); i++){
+        if(validTimes.get(i-1).overlaps(validTimes.get(i))){
+            //output.add(new TimeRange(validTimes.get(i-1).start(), validTimes.get(1).end() - validTimes.get(i-1).start()));
+        }
+    } 
+      return validTimes;
   }
 }
