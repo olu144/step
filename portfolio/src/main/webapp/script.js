eaec92691a33f5ce2bd29456538156c49f2b66a5
Olu <olu144@gmail.com>
2020-06-18 17:14:13 +0000

    change div name and add ev stations to sitewide navigation bar

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -226,7 +226,7 @@ function createMap() {
 function createEvChargingMap() {
   fetch("/ev-charging").then(response => response.json()).then((chargers) => {
     const map = new google.maps.Map(
-      document.getElementById('ev_map'),
+      document.getElementById("ev-map"),
       {center: {lat: 40.7128, lng: -74.0060}, zoom: 7,
       styles: [
       {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
