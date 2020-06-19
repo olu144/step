63f88259a1b8c9d71ec52efe10d201cfdb0cefcf
olu144 <54452415+olu144@users.noreply.github.com>
2020-06-19 15:46:25 -0400

    Chart.2 (#19)
    * complete step 1(servlets)
    
    * complete step 2(fetch)
    
    * part 2 complete(fetch)
    
    * modify DataServlet, script and index to complete step 3
    
    * complete step 4 by implementing form on contact page
    
    * complete the first part of week 3 step 5
    
    * Address requests made in 3.3
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * just in case
    
    * improved 3.5 with the addition of a comment class
    
    * just in case
    
    * complete week 3 step 6(additional features)
    
    * complete week 3 step 6(additional features)
    
    * formatting improvements
    
    * formatting improvements
    
    * address nits
    
    * add space
    
    * working form and minor UI tweaks
    
    * Week 4 login status servlet
    
    * improved HomeServlet
    
    * formatting
    
    * Viewing comments not requires a login
    
    * E-Mails are now displayed alongside comments
    
    * formatting
    
    * implemented chart in chart.html
    
    * formatting
    
    * test new charts with new data type
    
    * spelling
    
    * add api loading functions
    
    * optimize loadChartsApi function
    
    * remove commented code in script.js
    
    Co-authored-by: Olu <olu144@gmail.com>

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -93,14 +93,37 @@ function drawChart() {
   ]);
   const options = {
     'title': 'Zoo Animals',
+    'pieHole': 0.4,
     'width':500,
     'height':400
   };
-  const chart = new google.visualization.PieChart(document.getElementById('chart-container'));
+  const chart = new google.visualization.PieChart(document.getElementById('chart-div'));
   chart.draw(data, options);
 }
 
+function drawChart2() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'Week');
+  data.addColumn('number', 'Stock A');
+  data.addColumn('number', 'Stock B');
+  data.addRows([
+    [1,  50, 70],
+    [2,  130, 100],
+  ]);
+  var options = {
+    chart: {
+      title: 'Weekly Performcance of Stock A and Stock B',
+      subtitle: 'in dollars (USD)'
+    },
+    width: 900,
+    height: 500
+  };
+  var chart = new google.charts.Line(document.getElementById('linechart_material'));
+  chart.draw(data, google.charts.Line.convertOptions(options));
+}
+
 function loadChartsApi() {
-  google.charts.load('current', {'packages':['corechart']});
+  google.charts.load('current', {'packages':['corechart', 'line']});
   google.charts.setOnLoadCallback(drawChart);
+  google.charts.setOnLoadCallback(drawChart2);
 }
