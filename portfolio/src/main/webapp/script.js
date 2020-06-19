df5743a6d6518a654a5b292ce0a63ff623bd3230
Olu <olu144@gmail.com>
2020-06-19 19:53:29 +0000

    update function name in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -100,7 +100,7 @@ function drawStockChart() {
 }
 
 /** Fetches season data and uses it to create a chart. */
-function drawAnimalChart() {
+function drawSeasonChart() {
   fetch('/season-data').then(response => response.json())
   .then((seasonVotes) => {
     const data = new google.visualization.DataTable();
@@ -123,5 +123,5 @@ function drawAnimalChart() {
 function loadChartsApi() {
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawStockChart);
-  google.charts.setOnLoadCallback(drawAnimalChart);
+  google.charts.setOnLoadCallback(drawSeasonChart);
 }
