580f23863c564775592e0d00bc97b12bf14f2241
olu144 <54452415+olu144@users.noreply.github.com>
2020-06-29 11:24:21 -0400

    Tdd (#24)
    * complete step 1(servlets)
    
    * complete step 2(fetch)
    
    * part 2 complete(fetch)
    
    * modify DataServlet, script and index to complete step 3
    
    * complete step 4 by implementing form on contact page
    
    * complete the first part of week 3 step 5
    
    * Address requests made in 3.3
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * Complete part 2 of week 3 step 5
    
    * just in case
    
    * improved 3.5 with the addition of a comment class
    
    * just in case
    
    * complete week 3 step 6(additional features)
    
    * complete week 3 step 6(additional features)
    
    * formatting improvements
    
    * formatting improvements
    
    * address nits
    
    * add space
    
    * working form and minor UI tweaks
    
    * Week 4 login status servlet
    
    * improved HomeServlet
    
    * formatting
    
    * Viewing comments not requires a login
    
    * E-Mails are now displayed alongside comments
    
    * formatting
    
    * implemented chart in chart.html
    
    * formatting
    
    * test new charts with new data type
    
    * line chart included to display different data (stock prices)
    
    * formatting
    
    * add chart to display historical amazon stock data
    
    * formatting
    
    * add interactive data chart
    
    * remove servlet dependancy for season selection
    
    * fix commenting
    
    * add map viewer in maps.html and navbar tab in index.html
    
    * add custom map and map marker
    
    * add info window and click listener
    
    * Revert "add info window and click listener"
    
    This reverts commit 1736cd3bc865df5a7a205cf74c4e29f6acdb284f.
    
    * Revert "Revert "add info window and click listener""
    
    This reverts commit ce68415c2d0a94893f6fe410e211874c3da34b87.
    
    * Revert "Revert "Revert "add info window and click listener"""
    
    This reverts commit 3a25ca04bb01046bc694f91a84f2943cc769831c.
    
    * Revert "Revert "add info window and click listener""
    
    This reverts commit ce68415c2d0a94893f6fe410e211874c3da34b87.
    
    * add ev charging location map in ev.html
    
    * spelling
    
    * bring up to date
    
    * used amazon stock chart on stock picker page and modified map displayed on map.html
    
    * removed map.html, added map to about.html
    
    * add api loading functions
    
    * formatting
    
    * fix github + linkedin icons
    
    * edit function called in comments.html
    
    * fixed case of a in amazonData
    
    * optimize loadChartsApi function
    
    * chnaged function names
    
    * changed function names
    
    * added seasons servlet
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting
    
    * add perspective anlysis to recent comments on perspective.html
    
    * remove vars
    
    * add perspective functionality
    
    * new key structure/restrictions
    
    * tdd
    
    * tdd
    
    * tdd
    
    * tdd
    
    * ignoresPeopleNotAttending bug
    
    * addressed bug
    
    * formatting
    
    * optional attendee bug
    
    * addressed optional attendee bug
    
    * formatting + comments
    
    * change div name and add ev stations to sitewide navigation bar
    
    * GUI bug
    
    * Revert "GUI bug"
    
    This reverts commit b7a52ed9a9c928528196620c4974da29b540fd8b.
    
    * address nits(formatting and variable names)
    
    * address nits(clarify greeter and its testing, revise variable declaration, fix indentation)
    
    * findMeetingQueryChanges
    
    * simplify variable declerations
    
    * swap lines 46 and 47
    
    * migrated bug fix from tddWGUI
    
    * added getOptionalAttendees function to streamline query method
    
    * formatting
    
    Co-authored-by: Olu <olu144@gmail.com>

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,146 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> optionalAttendees = getOptionalAttendees(request);
+    Collection<String> requestAttendees = request.getAttendees();
+    // edge case: if there are no attendees the whole day is returned        
+    if (requestAttendees.isEmpty() && optionalAttendees.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    long requestDuration = request.getDuration();
+    // edge case: if the requested meeting is longer than a day there are no options
+    if (requestDuration >= TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();  
+    }
+    ArrayList<TimeRange> alreadyBookedTimes = getAlreadyBookedTimes(events, request);
+    ArrayList<TimeRange> alreadyBookedTimesWOptional = new ArrayList<TimeRange>();
+    if (!optionalAttendees.isEmpty()) {
+      alreadyBookedTimesWOptional = getAlreadyBookedTimesWithOptional(events, request, alreadyBookedTimes);
+    }
+    // if there are no meetings the whole day is returned 
+    if (alreadyBookedTimes.isEmpty() && alreadyBookedTimesWOptional.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);    
+    } 
+    ArrayList<TimeRange> noOverlap = removeOverlap(alreadyBookedTimes);
+    ArrayList<TimeRange> noOverlapWOptional = removeOverlap(alreadyBookedTimesWOptional);
+    ArrayList<TimeRange> availableTimes = addAvailableRanges(request, noOverlap);
+    ArrayList<TimeRange> availableTimesWOptional = addAvailableRanges(request, noOverlapWOptional);
+    // if there are no available times that include optional attendees return the list of available times that does not consider them
+    if (availableTimesWOptional.isEmpty()) {
+      return availableTimes;
+    }
+    return availableTimesWOptional;
+  }
+  
+  // if there are multiple meetings consolidate overlapping meetings into one range of time
+  public ArrayList<TimeRange> removeOverlap(ArrayList<TimeRange> times) {
+    ArrayList<TimeRange> noOverlap = new ArrayList<TimeRange>();
+    Collections.sort(times, TimeRange.ORDER_BY_START);
+    int newStart;
+    int newEnd;
+    if (times.size() > 1) {
+      for (int i = 1; i < times.size(); i++) {
+        if (times.get(i - 1).overlaps(times.get(i))) {
+          newStart = Math.min(times.get(i - 1).start(), times.get(i).start());
+          newEnd = Math.max(times.get(i).end(), times.get(i - 1).end());
+          if (!noOverlap.contains(TimeRange.fromStartEnd(newStart, newEnd, false))) {
+            noOverlap.add(TimeRange.fromStartEnd(newStart, newEnd, false));
+          }
+        } else {
+          noOverlap.add(times.get(i - 1));
+          noOverlap.add(times.get(i));
+        } 
+      }      
+    } else {
+      return times;
+    }
+    return noOverlap;
+  }
+
+  // add the times for all meetings where there are mandatory attendees
+  public ArrayList<TimeRange> getAlreadyBookedTimes(Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> alreadyBookedTimes = new ArrayList<TimeRange>();
+    Collection<String> requestAttendees = request.getAttendees();   
+    for (Event event: events) {
+      if (!event.getAttendees().isEmpty()) {
+        for (String attendee : requestAttendees) {
+          if (event.getAttendees().contains(attendee)) {
+            alreadyBookedTimes.add(event.getWhen());
+            break;
+          }
+        }
+      }
+    }
+    return alreadyBookedTimes;
+  }
+
+  // return the occupied time ranges including optional attendee schedules uses list of mandatory attendee time ranges to save time
+  public ArrayList<TimeRange> getAlreadyBookedTimesWithOptional(Collection<Event> events, MeetingRequest request, ArrayList<TimeRange> withoutOptional) {
+    Collection<String> optionalAttendees = request.getOptionalAttendees();   
+    ArrayList<TimeRange> alreadyBookedTimesWithOptional = new ArrayList<TimeRange>();
+    alreadyBookedTimesWithOptional.addAll(withoutOptional);
+    if (!optionalAttendees.isEmpty()) {
+      for (Event event : events) {
+        for (String attendee : optionalAttendees) {
+          if (event.getAttendees().contains(attendee)) {
+            alreadyBookedTimesWithOptional.add(event.getWhen());
+            break;
+          } 
+        }
+      } 
+    }
+    return alreadyBookedTimesWithOptional; 
+  }
+
+  // return all the time ranges that are not occupied 
+  public ArrayList<TimeRange> addAvailableRanges(MeetingRequest request, ArrayList<TimeRange> occupiedTimes) {
+    long requestDuration = request.getDuration();
+    ArrayList<TimeRange> availableRanges = new ArrayList<TimeRange>();
+    int newStart;
+    int newEnd;
+    if (occupiedTimes.isEmpty()) {
+      return occupiedTimes;
+    }
+    // if there is adequate time between the beginning of the day and the beginning of the first meeting add that time range to the output list   
+    if (occupiedTimes.get(0).start() - TimeRange.START_OF_DAY >= requestDuration) {
+      newStart = TimeRange.START_OF_DAY;
+      newEnd = occupiedTimes.get(0).start();
+      availableRanges.add(TimeRange.fromStartEnd(newStart, newEnd, false));    
+    }
+    // if there are adequate time between meetings add those time ranges to the output list
+    for (int i = 0; i < occupiedTimes.size() - 1; i++) {
+      if (occupiedTimes.get(i + 1).start() - occupiedTimes.get(i).end() >= requestDuration) {
+        newStart = occupiedTimes.get(i).end();
+        newEnd = occupiedTimes.get(i + 1).start();
+        availableRanges.add(TimeRange.fromStartEnd(newStart, newEnd, false));
+      }
+    }
+    // if there is adequate time between the end of the day and the end of the last meeting add that time range to the output list   
+    if (TimeRange.END_OF_DAY - occupiedTimes.get(occupiedTimes.size() - 1).end() >= requestDuration) {
+      newStart = occupiedTimes.get(occupiedTimes.size() - 1).end();
+      newEnd = TimeRange.END_OF_DAY;
+      availableRanges.add(TimeRange.fromStartEnd(newStart,newEnd , true));
+    }
+    return availableRanges;
+  }
+  
+  public Collection<String> getOptionalAttendees(MeetingRequest request) {
+    Collection<String> optionalAttendees = new ArrayList<String>();
+    // this try-catch avoids a potential NullPointerException
+    try {
+      optionalAttendees = request.getOptionalAttendees();
+    } catch(NullPointerException error) {
+    }
+    return optionalAttendees;  
   }
 }
